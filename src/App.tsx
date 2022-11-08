6a91945be50fb9332548fad5eb722e3c79120972
Patryk Sz <szabat.patryk@gmail.com>
2022-11-08 20:24:48 +0100

    post css + auprefixer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
 
   return (
-    <div className='h-full p-8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500'>
+    <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
       <h1 className='text-center text-5xl mb-4 font-display text-white'>Tic Tac Toe Game</h1>
+      <div className="">
+        <div className="text-white">Board Goes Here</div>
+        <div className="text-white">Score Goes Here</div>
+      </div>
     </div>
   )
 }
