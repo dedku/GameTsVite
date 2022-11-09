c5950282d48b04455d513f5d17b24e6ef4ad5e59
Patryk Sz <szabat.patryk@gmail.com>
2022-11-09 12:38:20 +0100

    game structure v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import { useState } from 'react'
+const INITIAL_GAME_STATE = ["X", "O", "X", "", "", "", "", "", ""]
 
+function App() {
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
   return (
     <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
       <h1 className='text-center text-5xl mb-4 font-display text-white'>Tic Tac Toe Game</h1>
       <div className="">
-        <div className="text-white">Board Goes Here</div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">{INITIAL_GAME_STATE.map((player, index) => (
+          <div key={index} className="h-36 border-solid border-4 border-skate-200 font-display text-7xl flex justify-center items-center cursor-pointer">{player}</div>
+        ))}</div>
         <div className="text-white">Score Goes Here</div>
       </div>
     </div>
